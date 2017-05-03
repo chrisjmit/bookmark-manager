6d022ad2b7d38106c9bc9a68129dc3f9378ac1d4
Sam <sblausten@Sams-Mac.local>
2017-05-03 15:43:36 +0100

    Heroku database setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,6 @@ class Link
 
 end
 
- DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+ DataMapper.setup(:default, "postgres://jfokbvgailmvzb:098fb4386bac8031b76cb58d8e9b52a3d957092100effe4f8790ee3bccf80d07@ec2-23-21-235-142.compute-1.amazonaws.com:5432/d4b201ltlpmn9c")
  DataMapper.finalize
  DataMapper.auto_upgrade!
