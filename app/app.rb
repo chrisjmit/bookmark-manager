3f0ea7a90b6b0ed2e731b5fe4388ec1e6d2cdf5f
Chris Mitchell <chrisjmit@gmail.com>
2017-05-04 12:30:15 +0100

    adds db config for blooming everglades

--- app/app.rb
+++ app/app.rb
@@ -17,11 +17,10 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
-    tag  = Tag.first_or_create(name: params[:tag]) 
-    link.tags << tag                      
-    link.save                           
+    tag  = Tag.first_or_create(name: params[:tag])
+    link.tags << tag
+    link.save
     redirect to('/links')
   end
 
 end
-
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,7 +5,7 @@ require "dm-migrations"
 require_relative 'models/tag'
 require_relative 'models/link'
 
-DataMapper.setup(:default, "postgres://jfokbvgailmvzb:098fb4386bac8031b76cb58d8e9b52a3d957092100effe4f8790ee3bccf80d07@ec2-23-21-235-142.compute-1.amazonaws.com:5432/d4b201ltlpmn9c")
+DataMapper.setup(:default, "postgres://ugvfwtkudnhbxi:d8ee15b2d11c52fa8662de1d09ec98ad09b37791e3732d382817298f249e7160@ec2-54-83-25-217.compute-1.amazonaws.com:5432/dceefoaibgnt6v")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 #|| ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"
